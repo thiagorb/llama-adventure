c3f86149bf22beebd1a87c72e0e22740987cb280
Thiago Barcala <thiago.barcala@gmail.com>
2019-08-19 08:43:10 +0200

    Replace getElementsByTagName with querySelector

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import * as sprites from './sprites';
 (async () => {
     await sprites.initialize();
 
-    const g = await game.create(document.getElementsByTagName('canvas')[0]);
+    const g = await game.create(document.querySelector('canvas'));
     game.start(g);
 })();
