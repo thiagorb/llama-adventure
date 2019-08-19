b8cc1ace6961469b976e7b0f23604ca673584e8c
Thiago Barcala <thiago.barcala@gmail.com>
2019-08-19 18:38:38 +0200

    Implement training room for tests

---
+++ src/training.ts
@@ -0,0 +1,53 @@
+import * as game from './game';
+import * as sprites from './sprites';
+import * as state from './state';
+import * as map from './map';
+import { deepCopy } from './utils';
+
+(async () => {
+    await sprites.initialize();
+
+    const levelMap = map.create({
+        rows: 15,
+        cols: 10,
+        values: new Int8Array([
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ])
+    });
+
+    const renderedMap = map.render(levelMap);
+    const gameState = state.create();
+    gameState.player.position = { x: 5, y: 10 };
+    gameState.goal.position = { x: 50, y: 50 };
+
+    const g: game.Game = {
+        canvas: document.querySelector('canvas'),
+        states: { current: gameState, next: deepCopy(gameState) },
+        stepsSinceBeginning: 0,
+        stepCount: 0,
+        frameCount: 0,
+        fps: 0,
+        sps: 0,
+        levelMap,
+        renderedMap,
+        secondInterval: null,
+        finished: false,
+        fadingOut: 0,
+    };
+
+    game.start(g);
+})();
