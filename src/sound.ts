eb98b5f7a5d84eadc07ee41c4d183a710a212514
Thiago Barcala <thiago.barcala@gmail.com>
2019-09-07 18:50:30 +0200

    Fix playing sounds concurrently

--- src/sound.ts
+++ src/sound.ts
@@ -12,30 +12,6 @@ type SoundPlayer = (oscillator: OscillatorNode, gain: GainNode, audio: AudioCont
 
 const dummyPlaySound = () => undefined;
 
-const createSound = (player: SoundPlayer) => cachedInstance(() => {
-    const audio = getAudio();
-
-    if (!audio) {
-        return dummyPlaySound;
-    }
-
-    const getGain = cachedInstance(() => {
-        const gain = audio.createGain();
-        gain.connect(audio.destination);
-        gain.gain.setValueAtTime(0, audio.currentTime);
-        return gain;
-    });
-
-    const getOscillator = cachedInstance(() => {
-        const oscillator = getAudio().createOscillator();
-        oscillator.connect(getGain());
-        oscillator.start();
-        return oscillator;
-    });
-
-    return () => player(getOscillator(), getGain(), getAudio())
-});
-
 export const getAudio = cachedInstance((): AudioContext => {
     if (typeof AudioContext !== 'undefined') {
         return new AudioContext();
@@ -50,15 +26,15 @@ export const getAudio = cachedInstance((): AudioContext => {
     return null;
 });
 
-const getJumpSound = createSound((oscillator, gain, audio) => {
+const jumpSound = (oscillator, gain, audio) => {
     oscillator.frequency.setValueAtTime(Notes.G4, audio.currentTime);
     oscillator.frequency.linearRampToValueAtTime(Notes.G5, audio.currentTime + 0.25);
     gain.gain.setValueAtTime(0, audio.currentTime);
     gain.gain.linearRampToValueAtTime(1, audio.currentTime + 0.01);
     gain.gain.linearRampToValueAtTime(0, audio.currentTime + 0.25);
-});
+};
 
-const getCollectSound = createSound((oscillator, gain, audio) => {
+const collectSound = (oscillator, gain, audio) => {
     oscillator.frequency.setValueAtTime(Notes.C5, audio.currentTime);
     oscillator.frequency.setValueAtTime(Notes.C7, audio.currentTime + 0.12);
     gain.gain.setValueAtTime(0, audio.currentTime);
@@ -66,8 +42,44 @@ const getCollectSound = createSound((oscillator, gain, audio) => {
     gain.gain.linearRampToValueAtTime(0, audio.currentTime + 0.8);
     gain.gain.linearRampToValueAtTime(1, audio.currentTime + 0.12);
     gain.gain.linearRampToValueAtTime(0, audio.currentTime + 0.5);
+};
+
+const getSoundPlayer = cachedInstance(() => {
+    const audio = getAudio();
+
+    if (!audio) {
+        return dummyPlaySound;
+    }
+
+    const createGain = () => {
+        const gain = audio.createGain();
+        gain.connect(audio.destination);
+        gain.gain.setValueAtTime(0, audio.currentTime);
+        return gain;
+    };
+
+    const createOscillator = (gain: GainNode) => {
+        const oscillator = getAudio().createOscillator();
+        oscillator.connect(gain);
+        oscillator.start();
+        return oscillator;
+    };
+
+    const MAX_CONCURRENT_SOUNDS = 10;
+    const nodes: Array<{ gain: GainNode, oscillator: OscillatorNode }> = [];
+    for (let i = 0; i < MAX_CONCURRENT_SOUNDS; i++) {
+        const gain = createGain();
+        nodes.push({ gain, oscillator: createOscillator(gain) });
+    }
+    let nextNode = 0;
+
+    return (player: SoundPlayer) => {
+        const node = nodes[nextNode];
+        nextNode = (nextNode + 1) % MAX_CONCURRENT_SOUNDS;
+        player(node.oscillator, node.gain, audio);
+    }
 });
 
-export const playJumpSound = () => getJumpSound()();
+export const playJumpSound = () => getSoundPlayer()(jumpSound);
 
-export const playCollectSound = () => getCollectSound()();
+export const playCollectSound = () => getSoundPlayer()(collectSound);
